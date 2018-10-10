24684d1deab95addb936dd8380abd1adf276f0a0
Jonathan Gray <jonathanceddy@gmail.com>
2018-10-10 13:56:38 -0700

    fix Product component tests

--- src/components/Product/Product.spec.js
+++ src/components/Product/Product.spec.js
@@ -1,27 +1,76 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
 import Product from '.'
 
-const setup = props => {
-  const component = shallow(
-    <Product {...props} />
+const productFactory = (
+  price = 9.99,
+  inventory = 6,
+  title = 'Test Product',
+) => ({
+  price,
+  inventory,
+  title,
+})
+
+const setup = (props = productFactory()) => {
+  const actions = {
+    onAddToCartClicked: jest.fn(),
+  }
+
+  const component = mount(
+    <Product {...props} {...actions} />
   )
 
   return {
-    component: component
+    actions,
+    component: component,
+    button: component.find('button'),
+    inventory: component.find('.product__body__inventory'),
   }
 }
 
 describe('Product component', () => {
-  it('should render title and price', () => {
-    const { component } = setup({ title: 'Test Product', price: 9.99 })
-    expect(component.text()).toBe('Test Product - $9.99')
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Product {...productFactory()} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
   })
 
   describe('when given inventory', () => {
-    it('should render title, price, and inventory', () => {
-      const { component } = setup({ title: 'Test Product', price: 9.99, inventory: 6 })
-      expect(component.text()).toBe('Test Product - $9.99 x 6')
+    it('should render inventory count', () => {
+      const { inventory } = setup()
+      expect(inventory.text()).toMatch(/^6 remaining/)
+    })
+
+    it('should trigger onAddToCartClicked', () => {
+      const { button, actions } = setup()
+      button.simulate('click')
+      expect(actions.onAddToCartClicked).toBeCalled()
+    })
+
+    it('should render "Add to cart" in button', () => {
+      const { button } = setup()
+      expect(button.text()).toMatch(/^Add to cart/)
+    })
+  })
+
+  describe('when given no inventory', () => {
+    it('should not trigger onAddToCartClicked', () => {
+      const { button, actions } = setup(productFactory(10, 0))
+      button.simulate('click')
+      expect(actions.onAddToCartClicked).not.toBeCalled()
+    })
+
+    it('should render "Sold Out" in button', () => {
+      const { button } = setup(productFactory(10, 0))
+      expect(button.text()).toMatch(/^Sold Out/)
+    })
+
+    it('should render no inventory', () => {
+      const { inventory } = setup(productFactory(10, 0))
+      expect(inventory.text()).toMatch(/^None remaining/)
     })
   })
 })
