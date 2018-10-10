9c4cd7f76c4fd6e711f8884c035510ec14b66c27
Jonathan Gray <jonathanceddy@gmail.com>
2018-10-10 11:11:54 -0700

    change api endpoint to https

--- src/api/shop.js
+++ src/api/shop.js
@@ -5,7 +5,7 @@
 const TIMEOUT = 100
 
 export default {
-  getProducts: cb => fetch("http://tech.work.co/shopping-cart/products.json")
+  getProducts: cb => fetch("https://tech.work.co/shopping-cart/products.json")
     .then(r => r.json())
     .then(cb),
   buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
