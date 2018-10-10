75ea02ab19b822ff3732fcf7ad30ed403aa1ee55
Jonathan Gray <jonathanceddy@gmail.com>
2018-10-10 14:20:21 -0700

    fix ProductsList tests

--- src/components/ProductsList/ProductsList.spec.js
+++ src/components/ProductsList/ProductsList.spec.js
@@ -1,27 +1,29 @@
 import React from 'react'
+import renderer from 'react-test-renderer'
 import { shallow } from 'enzyme'
 import ProductsList from '.'
 
 const setup = props => {
   const component = shallow(
-    <ProductsList title={props.title}>{props.children}</ProductsList>
+    <ProductsList>{props.children}</ProductsList>
   )
 
   return {
     component: component,
-    children: component.children().at(1),
-    h3: component.find('h3')
+    children: component.children().at(0),
   }
 }
 
 describe('ProductsList component', () => {
-  it('should render title', () => {
-    const { h3 } = setup({ title: 'Test Products' })
-    expect(h3.text()).toMatch(/^Test Products$/)
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<ProductsList>Test Children</ProductsList>)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
   })
 
   it('should render children', () => {
-    const { children } = setup({ title: 'Test Products', children: 'Test Children' })
+    const { children } = setup({ children: 'Test Children' })
     expect(children.text()).toMatch(/^Test Children$/)
   })
 })
