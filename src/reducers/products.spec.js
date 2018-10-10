c97f737bd82199c983b7d1734ac8d51430acd06c
Jonathan Gray <jonathanceddy@gmail.com>
2018-10-10 14:53:56 -0700

    add REMOVE_FROM_CART tests in products reducer

--- src/reducers/products.spec.js
+++ src/reducers/products.spec.js
@@ -1,6 +1,21 @@
 import reducer, * as products from './products'
 
-// TODO: handle REMOVE_FROM_CART action
+const productFactory = (
+  id = 1,
+  inventory = 2,
+  price = 10,
+  currency = 'USD',
+) => ({
+  id,
+  inventory,
+  price,
+  currency,
+  title: `Product ${id}`,
+  img: {
+    alt: `Product ${id} watch`,
+    src: 'test-file-stub'
+  },
+})
 
 describe('reducers', () => {
   describe('products', () => {
@@ -35,28 +50,8 @@ describe('reducers', () => {
       })
 
       it('contains the products from the action', () => {
-        expect(products.getProduct(state, 1)).toEqual({
-          currency: "USD",
-          id: 1,
-          img: {
-            alt: "Product 1 watch",
-            src: "test-file-stub"
-          },
-          inventory: 2,
-          price: 10,
-          title: "Product 1"
-        })
-        expect(products.getProduct(state, 2)).toEqual({
-          currency: "USD",
-          id: 2,
-          img: {
-            alt: "Product 2 watch",
-            src: "test-file-stub"
-          },
-          inventory: 1,
-          price: 10,
-          title: "Product 2"
-        })
+        expect(products.getProduct(state, 1)).toEqual(productFactory(1))
+        expect(products.getProduct(state, 2)).toEqual(productFactory(2, 1))
       })
 
       it ('contains no other products', () => {
@@ -65,64 +60,30 @@ describe('reducers', () => {
 
       it('lists all of the products as visible', () => {
         expect(products.getVisibleProducts(state)).toEqual([
-          {
-            currency: "USD",
-            id: 1,
-            img: {
-              alt: "Product 1 watch",
-              src: "test-file-stub"
-            },
-            inventory: 2,
-            price: 10,
-            title: "Product 1"
-          }, {
-            currency: "USD",
-            id: 2,
-            img: {
-              alt: "Product 2 watch",
-              src: "test-file-stub"
-            },
-            inventory: 1,
-            price: 10,
-            title: "Product 2"
-          }
+          productFactory(1),
+          productFactory(2, 1)
         ])
       })
 
       describe('when an item is added to the cart', () => {
-
         beforeEach(() => {
           state = reducer(state, { type: 'ADD_TO_CART', productId: 1 })
         })
 
         it('the inventory is reduced', () => {
           expect(products.getVisibleProducts(state)).toEqual([
-            {
-            currency: "USD",
-            id: 1,
-            img: {
-              alt: "Product 1 watch",
-              src: "test-file-stub"
-            },
-            inventory: 1,
-            price: 10,
-            title: "Product 1"
-          }, {
-            currency: "USD",
-            id: 2,
-            img: {
-              alt: "Product 2 watch",
-              src: "test-file-stub"
-            },
-            inventory: 1,
-            price: 10,
-            title: "Product 2"
-          }
+            productFactory(1, 1),
+            productFactory(2, 1)
           ])
         })
-
       })
 
+      describe('when an item is removed from the cart', () => {
+        it('returns the items back to products inventory', () => {
+          state = reducer(state, { type: 'REMOVE_FROM_CART', productId: 2, change: 2 })
+          expect(products.getProduct(state, 2).inventory).toBe(3)
+        })
+      })
     })
   })
 })
